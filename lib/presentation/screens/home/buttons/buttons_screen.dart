2745613aebf2ddcf5ee3e9bb6c6020ab74899a56
Gullit Moran <gullitmoran@hotmail.com>
2023-07-27 18:02:57 -0500

    Button Personalizado

--- lib/presentation/screens/home/buttons/buttons_screen.dart
+++ lib/presentation/screens/home/buttons/buttons_screen.dart
@@ -67,7 +67,7 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon(Icons.account_box_outlined), 
               label: const Text('Text Icons')),
 
-              // TODO: custom button
+            const CustomButton(),
 
             IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded)),
             IconButton(
@@ -80,6 +80,30 @@ class _ButtonsView extends StatelessWidget {
             ),
 
           ],
+        ),
+      ),
+    );
+  }
+}
+
+// Button personalizado
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return  ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child:  InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola Mundo', style: TextStyle(color: Colors.white),)),
         ),
       ),
     );
