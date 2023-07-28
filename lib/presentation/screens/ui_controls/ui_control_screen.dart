ee3b14f0a02b779ce998d64137d0189b0801a44a
Gullit Moran <gullitmoran@hotmail.com>
2023-07-27 19:00:01 -0500

    Screen creada y barril actualizado

---
+++ lib/presentation/screens/ui_controls/ui_control_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class UiControlScreen extends StatelessWidget {
+  
+  static const String name = 'ui_control_screen';
+  
+  const UiControlScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
