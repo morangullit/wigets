7d00f4ad11bdfc9ec9a619e68259b1aecca0dab4
Gullit Moran <gullitmoran@hotmail.com>
2023-07-31 11:02:30 -0500

    Progress Indicators Finish

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -29,4 +29,11 @@ const appMenuItems = <MenuItem>[
     link: '/cards', 
     icon: Icons.credit_card
     ),
+
+  MenuItem(
+    title: 'ProgressIndicators', 
+    subTitle: 'Generales y Controlados', 
+    link: '/progress', 
+    icon: Icons.refresh_rounded
+    ),
 ];
